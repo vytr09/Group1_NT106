cb5d6467002ead8bd4cdedd04ea68872c671adf0
hira <22521709@gm.uit.edu.vn>
2024-04-01 18:31:39 +0700

    update demo SOAP

---
+++ Demo/SOAP/WebApplication1/Group1Webservice.asmx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebApplication1
+{
+    [WebService(Namespace = "http://group1.com/nt106.o21.antt")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    public class Group1Webservice : System.Web.Services.WebService
+    {
+        [WebMethod]
+        public string HelloNT106()
+        {
+            return "Hello NT106.021.ANTT - From group 1";
+        }
+
+        [WebMethod]
+        public Person Hello(string name, int age)
+        {
+            return new Person
+            {
+                Name = name,
+                Age = age
+            };
+        }
+        [WebMethod]
+        public int sum(int a, int b)
+        {
+            return a + b;
+        }
+        [WebMethod]
+        public int sub(int a, int b)
+        {
+            return a - b;
+        }
+        [WebMethod]
+        public void end()
+        {
+            System.Environment.Exit(0);
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ Demo/SOAP/WebApplication1/HelloWorld.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="HelloWorld.asmx.cs" Class="WebApplication1.WebService1" %>
