646e325769865f10cfa02118e9eb359935eba3c6
hira <22521709@gm.uit.edu.vn>
2024-04-21 05:18:16 +0700

    update search

--- Project/iConnect/Search.cs
+++ Project/iConnect/Search.cs
@@ -16,5 +16,98 @@ namespace iConnect
         {
             InitializeComponent();
         }
+
+        private void guna2PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void postBtn_Click(object sender, EventArgs e)
+        {
+            postBtn.Checked = true;
+            allBtn.Checked = false;
+            recentBtn.Checked = false;
+            userBtn.Checked = false;
+
+            all2Pnl.Visible = false;
+            sort2Pnl.Visible = false;
+
+            postSortPnl.Visible = true;
+            userSortPnl.Visible = false;
+        }
+
+        private void allBtn_Click(object sender, EventArgs e)
+        {
+            postBtn.Checked = false;
+            allBtn.Checked = true;
+            recentBtn.Checked = false;
+            userBtn.Checked = false;
+
+            all2Pnl.Visible = true;
+            sort2Pnl.Visible = true;
+            postSortPnl.Visible = false;
+            userSortPnl.Visible = false;
+        }
+
+        private void recentBtn_Click(object sender, EventArgs e)
+        {
+            postBtn.Checked = false;
+            allBtn.Checked = false;
+            recentBtn.Checked = true;
+            userBtn.Checked =false;
+
+            all2Pnl.Visible = false;
+            sort2Pnl.Visible = false;
+            postSortPnl.Visible = true;
+            userSortPnl.Visible = false;
+        }
+
+        private void userBtn_Click(object sender, EventArgs e)
+        {
+            postBtn.Checked = false;
+            allBtn.Checked = false;
+            recentBtn.Checked = false;
+            userBtn.Checked = true;
+
+            all2Pnl.Visible = false;
+            sort2Pnl.Visible = false;
+            postSortPnl.Visible = false;
+            userSortPnl.Visible = true;
+        }
+
+        private void label29_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label31_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label30_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2Button13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2Button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label27_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void userSortPnl_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
