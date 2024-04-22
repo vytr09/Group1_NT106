ab3969b63bdf10d0c64c011862d3053a70ceeb9e
tuyen2201 <22521630@gm.uit.edu.vn>
2024-04-22 20:59:55 +0700

    add file

--- Demo/MultiThreading/Group1/Group1/obj/Release/net8.0/Group1.AssemblyInfo.cs
+++ Demo/MultiThreading/Group1/Group1/obj/Release/net8.0/Group1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Group1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe0c0ba8b7e11050fc9448342164d3bd16c9a836")]
 [assembly: System.Reflection.AssemblyProductAttribute("Group1")]
