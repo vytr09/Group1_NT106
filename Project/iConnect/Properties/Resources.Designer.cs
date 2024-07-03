c9fb766bfcb751228911ff8d18b9e95e722afead
hira <22521709@gm.uit.edu.vn>
2024-07-04 01:12:15 +0700

    update UI

--- Project/iConnect/Properties/Resources.Designer.cs
+++ Project/iConnect/Properties/Resources.Designer.cs
@@ -163,6 +163,16 @@ namespace iConnect.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap edit2 {
+            get {
+                object obj = ResourceManager.GetObject("edit2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap eye {
             get {
                 object obj = ResourceManager.GetObject("eye", resourceCulture);
