4d86edc5d9754825ee0087bb1dc57c404ef0cf41
hira <22521709@gm.uit.edu.vn>
2024-07-05 22:21:42 +0700

    update and maybe fix bugs

--- Project/iConnect/Helpers/LoadImageHttp.cs
+++ Project/iConnect/Helpers/LoadImageHttp.cs
@@ -19,13 +19,27 @@ namespace iConnect.Helpers
 
                 using (var stream = await response.Content.ReadAsStreamAsync())
                 {
-                    pictureBox.Image = Image.FromStream(stream);
+                    using (var originalImage = Image.FromStream(stream))
+                    {
+                        var resizedImage = ResizeImage(originalImage, pictureBox.Width, pictureBox.Height);
+                        pictureBox.Image = resizedImage;
+                    }
                 }
             }
             catch (Exception ex)
             {
                 MessageBox.Show($"Error loading image: {ex.Message}");
             }
+        }
+
+        private static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var resizedImage = new Bitmap(width, height);
+            using (var graphics = Graphics.FromImage(resizedImage))
+            {
+                graphics.DrawImage(image, 0, 0, width, height);
+            }
+            return resizedImage;
         }
     }
 }
